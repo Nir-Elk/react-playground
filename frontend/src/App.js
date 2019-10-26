a2d1235c95fff28f497b49f5bd95657404d09827
Aviv <avivsegal@gmail.com>
2019-10-26 21:59:51 +0300

    start multi lang

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,19 @@ import logo from './logo.svg';
 import Chat from './components/chat/Chat';
 import TicTacToe from './components/tic-tac-toe/TicTacToe';
 import Quiz from './components/quiz/Quiz';
-import MainPage from './components/mainPage/MainPage'
+import MainPage from './components/mainPage/MainPage';
+import english from './dictionaries/english';
+import hebrew from './dictionaries/hebrew';
+import LangContext from './LangContext'
+
+const codeToDic = {en:english,heb:hebrew};
 
 class App extends React.Component {
 
     constructor(props, context) {
         super(props, context);
-        this.state = {chatState: null,ticTacToeState: null, quizState:null};
+        this.state = {chatState: null, ticTacToeState: null, quizState: null, lang: "en"};
+
         this.notify = this.notify.bind(this);
     }
 
@@ -19,42 +25,58 @@ class App extends React.Component {
         this.setState(someState);
     }
 
+
+
     render() {
         return (
-            <Router>
-                <Navbar bg="dark" variant="dark">
+            <LangContext.Provider value={codeToDic[this.state.lang]}>
+                <Router>
+                    <Navbar bg="dark" variant="dark">
 
-                    <Link to={'/'}>
-                    <Navbar.Brand>
-                        <img
-                            alt=""
-                            src={logo}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
-                        />
-                        {' React Playground'}
-                    </Navbar.Brand>
-                </Link>
+                        <Link to={'/'}>
+                            <Navbar.Brand>
+                                <img
+                                    alt=""
+                                    src={logo}
+                                    width="30"
+                                    height="30"
+                                    className="d-inline-block align-top"
+                                />
+                                {dictionary.siteName}
+                            </Navbar.Brand>
+                        </Link>
+                        <Nav className="ml-auto pr-md-5" style={{marginRight: '30px'}}>
+                            <NavDropdown title={dictionary.menu.title} id="basic-nav-dropdown">
+                                {
+                                    ["chat", "ticTacToe", "quiz"].map(item =>
+                                        <NavDropdown.Item
+                                            href={'/' + item}>{dictionary.menu.items[item]}</NavDropdown.Item>)
+                                }
+                            </NavDropdown>
+                        </Nav>
+                        <Nav className="ml-auto pr-md-5" style={{marginRight: '30px'}}>
+                            <NavDropdown title={dictionary.langMenu.title} id="basic-nav-dropdown">
+                                {
+                                    ["en", "heb"].map(code =>
+                                        <NavDropdown.Item onClick={() => {
+                                            console.log(code);
+                                        }}>{dictionary.langMenu.items[code]}</NavDropdown.Item>)
+                                }
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar>
 
-                    <Nav className="ml-auto pr-md-5" style={{marginRight:'30px'}}>
-                        <NavDropdown title="Projects" id="basic-nav-dropdown">
-                            <NavDropdown.Item href={'/chat'}>Chat</NavDropdown.Item>
-                            <NavDropdown.Item href={'/tictactoe'}>Tic Tac Toe</NavDropdown.Item>
-                            <NavDropdown.Item href={'/quiz'}>Quiz</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar>
+                    <Route exact path={'/'} component={MainPage}/>
+                    <Route path={'/chat'}
+                           component={() => <Chat initialState={this.state.chatState} notifyApp={this.notify}/>}/>
+                    <Route path={'/ticTacToe'}
+                           component={() => <TicTacToe initialState={this.state.ticTacToeState}
+                                                       notifyApp={this.notify}/>}/>
+                    <Route path={'/quiz'}
+                           component={() => <Quiz initialState={this.state.quizState} notifyApp={this.notify}/>}/>
 
-                <Route exact path={'/'} component={MainPage}/>
-                <Route path={'/chat'}
-                       component={() => <Chat initialState={this.state.chatState} notifyApp={this.notify}/>}/>
-                <Route path={'/tictactoe'}
-                       component={() => <TicTacToe initialState={this.state.ticTacToeState} notifyApp={this.notify}/>}/>
-                <Route path={'/quiz'}
-                       component={() => <Quiz initialState={this.state.quizState} notifyApp={this.notify}/>}/>
-
-            </Router>
+                </Router>
+            </LangContext.Provider>
         );
     }
 }
